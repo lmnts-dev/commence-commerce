55f41b7310e24535674430d1af734a0fd9bf1089
Jose Rios <jose@commence.studio>
2023-04-18 17:15:39 -0700

    :rocket: Add js file

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -7,3 +7,8 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+function  my_theme_enqueue_scripts() {
+    wp_enqueue_script('custom', get_stylesheet_directory_uri().'/js/theme.js', 
+    array(), false, true);
+}
